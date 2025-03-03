f4e1105bda088453156357c397fade83b09e1ff3
Sayali Wadekar <sallywdkr1111@gmail.com>
2025-03-03 18:28:56 +0530

    Fixed wsgi.py by removing duplicate and incorrect lines

--- BookPalace/wsgi.py
+++ BookPalace/wsgi.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 """
 WSGI config for BookPalace project.
 
@@ -9,27 +8,8 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BookPalace.settings')
 
 application = get_wsgi_application()
-=======
-"""
-WSGI config for BookPalace project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BookPalace.settings')
-
-application = get_wsgi_application()
->>>>>>> 2c9025f (Initial commit with requirements.txt)
